cb7ae9462a6917c179b96f95c0c8ecd79ee7c044
dreamprogrammin <mbabazov@list.ru>
2025-02-20 08:04:58 +0500

    settings pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  modules: [
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/supabase",
+  ],
+  plugins: ["~/plugins/pinia-persistedstate.client.ts"],
+  devtools: { enabled: true },
+});
