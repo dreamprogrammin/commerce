b81246c6dd679b5492bb9a37accd79f3adec92e1
malikzzz <mbabazov@list.ru>
2025-02-24 18:03:27 +0600

    setting config supabase

---
+++ plugins/supabase.client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+export default defineNuxtPlugin ((nuxtApp) => {
+    const config = useRuntimeConfig()
+
+    const supabase = createClient(config.public.supabaseUrl, config.public.supabaseKey)
+
+    return nuxtApp.provide('supabase', supabase)
+})
