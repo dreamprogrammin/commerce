355f73d588de4d221b9060530b04a002b1612495
malikzzz <mbabazov@list.ru>
2025-03-07 15:53:57 +0600

    final user

--- pages/confirm/index.vue
+++ pages/confirm/index.vue
@@ -1,38 +1,30 @@
 <script setup lang="ts">
-const supabase = useSupabaseClient();
-const route = useRoute();
-const client = useSupabaseUser();
+const user = useSupabaseUser();
 
-const error = ref("");
-const confirm = ref(false);
-const isLoading = ref(false);
-
-async function resendConfirm() {
-  try {
-    if (!client.value?.email) {
-      throw new Error("Ошибка не авторизованный пользователь");
+watch(
+  user,
+  () => {
+    if (user.value) {
+      return navigateTo("/dashboard");
     }
-    const {} = await supabase.auth.resend({
-      type: "signup",
-      email: client.value?.email,
-    });
-  } catch (error) {
-  } finally {
+  },
+  {
+    immediate: true,
   }
-}
+);
 </script>
 <template>
   <div>
     <h1>Страница для подтверждение почты</h1>
 
-    <div>
+    <div v-if="user">
       <h2>Email подтвержден</h2>
       <nuxt-link to="/dashboard">Перейти на рабочую страницу</nuxt-link>
     </div>
 
-    <div>
+    <div v-else>
       <h2>Email не подтвержден</h2>
-      <p>{{}}</p>
+
       <button>Отправить письмо повторно</button>
     </div>
   </div>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+async function handleOut() {
+  await authStore.handleOut();
+  await navigateTo("/");
+}
+</script>
 <template>
   <div>
     <h1>Страница для авторизованных и подтвержденных пользователей</h1>
+    <div><nuxt-link to="/">На главную</nuxt-link></div>
+    <div>
+      <button @click="handleOut">Выйти с аккаунта:</button>
+    </div>
   </div>
 </template>
