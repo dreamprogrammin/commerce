4db59cbc40bc6510a63a2d1d538809f9b942f7ea
dreamprogrammin <mbabazov@list.ru>
2025-04-07 08:02:36 +0500

    settings supabase gen types

--- utils/userCheckEmail.ts
+++ utils/userCheckEmail.ts
@@ -1,5 +1,5 @@
-export async function checkEmail (email:string) {
-    const supabase = useSupabaseClient()
-    const {data, error} = await supabase.from('auth.users').select('email').eq('email', email)
-    console.log(data)
+export async function checkEmail(email: string) {
+  const supabase = useSupabaseClient();
+  const { data, error } = await supabase.from('auth.users').select('email').eq('email', email);
+  console.log(data);
 }
