c099764c4cf76dc81077bf86c007f6f180ea06e5
dreamprogrammin <mbabazov@list.ru>
2025-04-28 08:04:54 +0500

    create structure project

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <header class="header">
+    <div>
+      <div>LOGO</div>
+
+      <button>Войти</button>
+      <button>Корзина</button>
+      <button>Кешбэк</button>
+    </div>
+    <slot />
+  </header>
+</template>
+
+<style scoped></style>
