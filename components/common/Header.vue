71eccc05aac8bc8c1599fbdfe6391c27b32dfd04
dreamprogrammin <mbabazov@list.ru>
2025-04-30 08:03:57 +0500

    created layout profile

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-import { useAuth } from '~/composables/auth/useAuth';
+import { useAuth } from "~/composables/auth/useAuth";
 
-const {handleAuthGoogle} = useAuth()
+const { handleAuthGoogle } = useAuth();
 </script>
 <template>
-      <header class="header">
-    <div>
+  <header class="header">
+    <div class="wrapper">
       <div>
         <nuxt-link to="/">Logo</nuxt-link>
       </div>
 
-      <nuxt-link to="/profile">Профиль</nuxt-link>
-      <button>Корзина</button>
-      <button>Кешбэк</button>
+      <div>
+        <nuxt-link to="/profile">Профиль</nuxt-link>
+        <button>Корзина</button>
+        <button>Кешбэк</button>
+      </div>
     </div>
   </header>
 </template>
+<style>
+header .wrapper {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15px;
+  max-width: 1350px;
+}
+</style>
