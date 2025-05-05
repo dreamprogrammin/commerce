7b91a6531c38b033683b293cecad0932b9d80a5e
malikzzz <mbabazov@list.ru>
2025-05-05 17:59:35 +0600

    settings middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,37 +3,19 @@ import { useModalStore } from "~/stores/modal/useModalStore";
 export default defineNuxtRouteMiddleware((to, _from) => {
   const user = useSupabaseUser();
   const modalStore = useModalStore();
-  const publicRoutes = [
-    "/",
-    "/login",
-    "/register",
-    "/confirm",
-    "/forgot-password",
-    "/reset-password",
-    "/profile",
-  ];
+  const dashboard = '/dashboard'
   const profilePath = [
     "/profile/bonus",
     "/profile/favorites",
     "/profile/order",
     "/profile/settings",
   ];
-  // if (user.value === null) {
-  //   return navigateTo("/login");
-  // }
-  if (user.value && !user.value?.confirmed_at) {
-    return navigateTo("/confirm");
-  }
-
-  if (!user.value && !publicRoutes.includes(to.path)) {
+  if (!user.value && to.path === dashboard) {
     return navigateTo("/login");
   }
 
   if (user.value && ["/login", "/register"].includes(to.path)) {
-    return navigateTo("/dashboard");
-  }
-  if (to.path === "/profile" && !user.value) {
-    return navigateTo("/login");
+    return navigateTo("/");
   }
   if (profilePath.includes(to.path) && !user.value) {
     modalStore.openLoginModal();
