8239f5507cfd5f6589205dcd45e98a04762d927a
dreamprogrammin <mbabazov@list.ru>
2025-06-21 09:35:27 +0500

    updated

--- components/common/TabBar.vue
+++ components/common/TabBar.vue
@@ -501,6 +501,19 @@ onMounted(async () => {
                           {{ child.title }}
                         </div>
                       </NuxtLink>
+                      <ul>
+                        <li v-for="child3 in menuItemsStore.getChildren(child.slug)" :child3.slug>
+                            <NuxtLink
+                        :to="child3.href || '#'"
+                        class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                        @click="activeMenuValue = undefined"
+                      >
+                      <div class="text-sm font-medium leading-none">
+                          {{ child3.title }}
+                        </div>
+                      </NuxtLink>
+                        </li>
+                      </ul>
                     </li>
                   </ul>
                 </div>
