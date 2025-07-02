45196b70be3bff5b432068528a5cf1b67ab47723
malikzzz <mbabazov@list.ru>
2025-07-02 16:45:46 +0600

    change type

--- types/type.ts
+++ types/type.ts
@@ -20,7 +20,7 @@ export interface IParamsForgotPassword {
 
 export interface IProfile {
   id: string;
-  email: string | null;
+  email?: string | null;
   first_name: string | null;
   last_name: string | null;
   phone: string | null;
