358b27fe2950bec80a53797c5b6cff7f43049ff2
Малик <81822375+dreamprogrammin@users.noreply.github.com>
2025-08-24 17:49:56 +0500

    Dev (#172)
    * add (#150)
    
    * Feature/seamless guest (#151)
    
    * added migration
    
    * updated types supabase
    
    * feat(updated store)
    
    * feat(updated)
    
    * feat(updated checkout page) (#154)
    
    * feat(updated checkout page part 2) (#156)
    
    * Feature/welcome bonus system (#158)
    
    * feat(added new logic bonus)
    
    * fix(stores): correct typings and async logic
    
    * updated images (#160)
    
    * updated images
    
    * fix code
    
    * fix code part 2
    
    * Feature/image optimization (#162)
    
    * updated images
    
    * fix code
    
    * fix code part 2
    
    * fix code part 3
    
    * Feature/image optimization (#164)
    
    * updated images
    
    * fix code
    
    * fix code part 2
    
    * fix code part 3
    
    * Dev (#163)
    
    * add (#150)
    
    * Feature/seamless guest (#151)
    
    * added migration
    
    * updated types supabase
    
    * feat(updated store)
    
    * feat(updated)
    
    * feat(updated checkout page) (#154)
    
    * feat(updated checkout page part 2) (#156)
    
    * Feature/welcome bonus system (#158)
    
    * feat(added new logic bonus)
    
    * fix(stores): correct typings and async logic
    
    * updated images (#160)
    
    * updated images
    
    * fix code
    
    * fix code part 2
    
    * Feature/image optimization (#162)
    
    * updated images
    
    * fix code
    
    * fix code part 2
    
    * fix code part 3
    
    * fix code part 4
    
    * fix (#166)
    
    * fix code part 6 (#168)
    
    * fix code part 6
    
    * fix code part 7
    
    * fix code part 8 (#171)

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const BUCKET_NAME = 'menu-item-images'
-export const BUCKET_NAME_PRODUCT = 'products-images'
+export const BUCKET_NAME_PRODUCT = 'product-images'
 export const NO_PARENT_OPTION_NAME = ''
 export const NO_PARENT_SLUG_VALUE_FOR_SELECT = '__NO_PARENT__'
