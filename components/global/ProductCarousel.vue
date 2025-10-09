2c633f349c6f95188329471e8f9c005d33328bf6
dreamprogrammin <mbabazov@list.ru>
2025-10-10 00:20:50 +0500

    updated visual productCarousel on the home page

--- components/global/ProductCarousel.vue
+++ components/global/ProductCarousel.vue
@@ -9,20 +9,21 @@ defineProps<{
 
 <template>
   <section v-if="products && products.length > 0" class="container py-8 md:py-12">
-    <slot name="header" />
+    <div class="container">
+      <slot name="header" />
+    </div>
 
     <Carousel
       class="w-full"
       :opts="{
-        align: 'start',
-        loop: products.length > 4,
+        loop: products.length > 5,
       }"
     >
       <CarouselContent class="-ml-4">
         <CarouselItem
           v-for="product in products"
           :key="product.id"
-          class="pl-4 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+          class="pl-4 w-[45%] sm:w-[30%] md:w-[22%] lg:w-[18%]"
         >
           <div class="p-1 h-full">
             <ProductCard :product="product" class="h-full" />
