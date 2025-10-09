f3772371a5a246e83ab69e00c3b85f04f0df0cd9
malikzzz <mbabazov@list.ru>
2025-10-09 16:55:15 +0600

    updated visual on the mobiles productGrid

--- components/global/ProductGrid.vue
+++ components/global/ProductGrid.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+  <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
     <ProductCard
       v-for="product in products"
       :key="product.id"
