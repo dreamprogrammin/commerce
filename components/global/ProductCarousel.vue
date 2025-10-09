dbc5f90cdba479adf21e4625a3a48241cb7fabb6
dreamprogrammin <mbabazov@list.ru>
2025-10-10 00:29:14 +0500

    updated visual productCarousel on the home page fix

--- components/global/ProductCarousel.vue
+++ components/global/ProductCarousel.vue
@@ -8,30 +8,46 @@ defineProps<{
 </script>
 
 <template>
-  <section v-if="products && products.length > 0" class="container py-8 md:py-12">
+  <section v-if="products && products.length > 0" class="py-8 md:py-12">
+    <!-- `container` нужен только для заголовка -->
     <div class="container">
       <slot name="header" />
     </div>
 
-    <Carousel
-      class="w-full"
-      :opts="{
-        loop: products.length > 5,
-      }"
-    >
-      <CarouselContent class="-ml-4">
-        <CarouselItem
-          v-for="product in products"
-          :key="product.id"
-          class="pl-4 w-[45%] sm:w-[30%] md:w-[22%] lg:w-[18%]"
-        >
-          <div class="p-1 h-full">
-            <ProductCard :product="product" class="h-full" />
-          </div>
-        </CarouselItem>
-      </CarouselContent>
-      <!-- <CarouselPrevious class="hidden sm:inline-flex" />
-      <CarouselNext class="hidden sm:inline-flex" /> -->
-    </Carousel>
+    <!--
+      Обертка `overflow-hidden` важна, чтобы спрятать горизонтальный скроллбар,
+      который может появиться из-за отрицательного margin.
+    -->
+    <div class="overflow-hidden">
+      <Carousel
+        class="w-full"
+        :opts="{
+          align: 'start',
+          loop: products.length > 5,
+        }"
+      >
+        <!--
+          `pl-4 sm:pl-6 lg:pl-8` - этот padding должен соответствовать padding'у вашего .container,
+          чтобы первый элемент был выровнен по левому краю.
+          `-ml-4` компенсирует `pl-4` на `CarouselItem`.
+        -->
+        <CarouselContent class="-ml-4 pl-4 sm:pl-6 lg:pl-8">
+          <CarouselItem
+            v-for="product in products"
+            :key="product.id"
+            class="pl-4
+                   basis-2/3
+                   sm:basis-1/2
+                   md:basis-1/3
+                   lg:basis-1/4
+                   xl:basis-1/5"
+          >
+            <div class="p-1 h-full">
+              <ProductCard :product="product" class="h-full" />
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+    </div>
   </section>
 </template>
