8c20a41ebe8ca7dcfde8b131bccb9bc4618ef7d9
malik <mbabazov@list.ru>
2026-06-29 13:39:54 +0500

    fix: добавлена обработка ошибок в kaspi feed

--- server/api/kaspi-feed.xml.ts
+++ server/api/kaspi-feed.xml.ts
@@ -1,35 +1,56 @@
 export default defineEventHandler(async (event) => {
-  const client = await serverSupabaseClient(event)
-  
-  const { data: products, error } = await client.rpc('get_kaspi_feed_products')
-  
-  if (error) {
-    console.error('Kaspi feed error:', error)
-    throw createError({ statusCode: 500, message: error.message })
-  }
-
-  if (!products || products.length === 0) {
-    console.warn('No products for Kaspi feed')
-  }
-
-  const currentDate = new Date().toISOString().split('T')[0]
-  
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<kaspi_catalog date="${currentDate}" xmlns="kaspiShopping" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="kaspiShopping http://kaspi.kz/kaspishopping.xsd">
+  try {
+    const client = await serverSupabaseClient(event)
+    
+    const { data: products, error } = await client.rpc('get_kaspi_feed_products')
+    
+    if (error) {
+      console.error('Kaspi feed RPC error:', error)
+      // Возвращаем пустой фид вместо ошибки
+      const emptyXml = `<?xml version="1.0" encoding="UTF-8"?>
+<kaspi_catalog date="${new Date().toISOString().split('T')[0]}" xmlns="kaspiShopping" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="kaspiShopping http://kaspi.kz/kaspishopping.xsd">
   <company>Ухтышка</company>
   <merchantid>YOUR_MERCHANT_ID</merchantid>
-  <offers>
-${(products || []).map(p => `    <offer sku="${p.sku}">
-      <model><![CDATA[${p.name}]]></model>
+  <offers></offers>
+</kaspi_catalog>`
+      setResponseHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
+      return emptyXml
+    }
+
+    const currentDate = new Date().toISOString().split('T')[0]
+    
+    const offersXml = (products || []).map(p => {
+      return `    <offer sku="${p.sku || ''}">
+      <model><![CDATA[${p.name || ''}]]></model>
       <brand><![CDATA[${p.brand_name || ''}]]></brand>
       <availabilities>
         <availability available="${p.stock_quantity > 0 ? 'yes' : 'no'}" storeId="PP1" />
       </availabilities>
-      <price>${p.kaspi_price}</price>
-    </offer>`).join('\n')}
+      <price>${p.kaspi_price || 0}</price>
+    </offer>`
+    }).join('\n')
+    
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<kaspi_catalog date="${currentDate}" xmlns="kaspiShopping" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="kaspiShopping http://kaspi.kz/kaspishopping.xsd">
+  <company>Ухтышка</company>
+  <merchantid>YOUR_MERCHANT_ID</merchantid>
+  <offers>
+${offersXml}
   </offers>
 </kaspi_catalog>`
 
-  setResponseHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
-  return xml
+    setResponseHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
+    return xml
+  } catch (err) {
+    console.error('Kaspi feed general error:', err)
+    // Возвращаем минимальный валидный XML
+    const errorXml = `<?xml version="1.0" encoding="UTF-8"?>
+<kaspi_catalog date="${new Date().toISOString().split('T')[0]}" xmlns="kaspiShopping" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="kaspiShopping http://kaspi.kz/kaspishopping.xsd">
+  <company>Ухтышка</company>
+  <merchantid>YOUR_MERCHANT_ID</merchantid>
+  <offers></offers>
+</kaspi_catalog>`
+    setResponseHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
+    return errorXml
+  }
 })
